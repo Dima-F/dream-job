3dca299c339747a0fd2ddce6f7216862f739647d
f4o <dmytro@git.colab.local>
2025-08-13 19:02:57 +0300

    start templating

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -15,7 +16,11 @@ func main() {
 	logConfig := config.NewLogConfig()
 	customLogger := logger.NewLogger(logConfig)
 
-	app := fiber.New()
+	engine := html.New("./html", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
 	}))
--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,9 +1,6 @@
 package home
 
 import (
-	"bytes"
-	"html/template"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
@@ -14,14 +11,19 @@ type HomeHandler struct {
 }
 
 func (h *HomeHandler) home(c *fiber.Ctx) error {
-	teml := template.Must(template.ParseFiles("./html/page.html"))
-	data := struct{ Count int }{Count: 1}
-	var tpl bytes.Buffer
-	if err := teml.Execute(&tpl, data); err != nil {
-		return fiber.NewError(fiber.StatusBadRequest, "Template compile error")
-	}
-	c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
-	return c.Send(tpl.Bytes())
+	// teml := template.Must(template.ParseFiles("./html/page.html"))
+	// data := struct{ Count int }{Count: 1}
+	// var tpl bytes.Buffer
+	// if err := teml.Execute(&tpl, data); err != nil {
+	// 	return fiber.NewError(fiber.StatusBadRequest, "Template compile error")
+	// }
+	// c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+	// return c.Send(tpl.Bytes())
+	return c.Render("page", fiber.Map{
+		"Count":   3,
+		"IsAdmin": true,
+		"CanUse":  false,
+	})
 }
 
 func (h *HomeHandler) error(c *fiber.Ctx) error {
