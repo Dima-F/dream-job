f0c9dddb5ebccc0e2ccf4544a309f4304ac348f3
Dima-F <dfiialo@yahoo.com>
2025-09-06 14:55:29 +0300

    start deeper htmx, validation

--- internal/vacancy/handler.go
+++ internal/vacancy/handler.go
@@ -1,6 +1,8 @@
 package vacancy
 
 import (
+	"github.com/Dima-F/dream-job/pkg/tadapter"
+	"github.com/Dima-F/dream-job/views/components"
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
@@ -20,5 +22,14 @@ func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 }
 
 func (h *VacancyHandler) createVacancy(c *fiber.Ctx) error {
-	return c.SendString("create vacancy...")
+	email := c.FormValue("email")
+	// h.customLogger.Info().Msg(email)
+
+	if email == "" {
+		failComponent := components.Notification("Something get wrong", components.NotificationFail)
+		return tadapter.Render(c, failComponent)
+	}
+
+	successComponent := components.Notification("Vacancy created successfully", components.NotificationSuccess)
+	return tadapter.Render(c, successComponent)
 }
